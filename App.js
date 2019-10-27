17ede106814302ebb3e7e6823ff7162f1ec9eb1d
Ajdin <ahmetovicajdin@gmail.com>
2019-10-27 22:28:14 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Clarifai from 'clarifai'
+
+
+
+const clarifaiAPI = new Clarifai.App({
+  apiKey: 'f36318657930429086a26abcbcda20be'
+ });
+
+class App extends React.Component {
+
+  
+  constructor (props){
+    super(props)
+
+  }
+
+  render (){
+    return(
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.base64);
+
+      clarifaiAPI.models.predict(Clarifai.FOOD_MODEL, {base64: data.base64}).then(
+        function(response) {
+          console.log(response)
+        },
+        function(err) {
+          console.log(err)
+        }
+      );
+
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+  
+});
+
+export default App;
