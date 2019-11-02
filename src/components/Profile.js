6d69d3b7ae23d9c4333561e1fefa86cbe7c1163e
Almir Subasic <subashaaa@pc.mshome.net>
2019-11-02 08:56:37 +0100

    pokusaj responsive

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -32,28 +32,28 @@ class Profile extends React.Component {
            <Text style={{fontFamily: 'Montserrat', fontSize: 16,alignSelf: 'center',color: 'gray'}}>Tuzla, 75000</Text>
         </View> 
 
-        <View style={{flex: 1,flexDirection: 'row',top:'30%'}}>
-            <Image style={{left:50, width: 100,height: 100}} source={require('../assets/star.png')}/>
+        <View style={{flex: 1, flexDirection: 'row', top:'30%', left:'8%'}}>
+            <Image style={{left: '430%', width: 100, height: 100}} source={require('../assets/star.png')}/>
             <View style={{width: 75}}/>
             <Image style={{height: 120}} source={require('../assets/line.png')}/>
             <View style={{width: 25}}/>
-            <Image style={{width: 100,height: 100}} source={require('../assets/doc.png')}/>
+            <Image style={{width: 100, height: 100, left: '20%'}} source={require('../assets/doc.png')}/>
         </View>
-        <View style={{flex: 1,flexDirection: 'row',top:'50%'}}>
-            <Text style={{left: 40, width: 120,height: 120,fontWeight: 'bold',fontSize: 25,color: 'gray'}}>365 <Text style={{width: 120,height: 120,fontSize: 25,fontWeight: 'normal'}}>points</Text></Text>
+        <View style={{flex: 1, flexDirection: 'row', top:'50%'}}>
+            <Text style={{left: '600%', width: 120,height: 120,fontWeight: 'bold',fontSize: 25,color: 'gray'}}>365 <Text style={{width: 120,height: 120,fontSize: 25,fontWeight: 'normal'}}>points</Text></Text>
             <View style={{width: 30}}/>
-            <Text style={{left: 50, width: 120,height: 120,fontWeight: 'bold',fontSize: 25,color: 'gray'}}>17 <Text style={{width: 120,height: 120,fontSize: 25,fontWeight: 'normal'}}>recipes</Text></Text>
+            <Text style={{left: '700%', width: 120,height: 120,fontWeight: 'bold',fontSize: 25,color: 'gray'}}>17 <Text style={{width: 120,height: 120,fontSize: 25,fontWeight: 'normal'}}>recipes</Text></Text>
         </View>
-        <View style={{flex: 1,flexDirection: 'row',top:'55%'}}>
+        <View style={{flex: 1, flexDirection: 'row', top:'55%'}}>
 
           <TouchableOpacity style={{position: 'absolute'}}>
-            <Text style={{left: 70, width: 120,height: 120,fontSize: 13,color: 'gray',textDecorationLine: 'underline',color: 'red'}}>earn more</Text>
+            <Text style={{left: '80%', width: 120,height: 120,fontSize: 13,color: 'gray',textDecorationLine: 'underline',color: 'red'}}>earn more</Text>
           </TouchableOpacity>
             
           <View style={{width: 50}}/>
           
           <TouchableOpacity style={{position: 'absolute', left: 230}}>
-            <Text style={{width: 120,height: 120,fontSize: 13,color: 'gray', textDecorationLine: 'underline',color: 'red'}}>write more</Text>
+            <Text style={{left: '18%', width: 120,height: 120,fontSize: 13,color: 'gray', textDecorationLine: 'underline',color: 'red'}}>write more</Text>
           </TouchableOpacity>
 
         </View>
